d179d6cf537ce573f3b6727b4dd3e87511186b33
Ricardo Malagon <ricardo@ricardomalagon.com>
2021-02-17 11:44:41 -0400

    first commit

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,10 +1,23 @@
 import React from 'react'
+import axios from 'axios'
 
-import { ExampleComponent } from '@upnread/post-renderer'
-import '@upnread/post-renderer/dist/index.css'
+import PostReader from '@upnread/post-renderer'
 
 const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
+  const [post, setPost] = React.useState({ body: '' })
+
+  React.useEffect(() => {
+    async function getPost() {
+      const { data } = await axios.get(
+        'http://127.0.0.1:8000/api/post/test_post'
+      )
+      setPost(data)
+    }
+
+    getPost()
+  }, [])
+
+  return <PostReader post={post.body} />
 }
 
 export default App
