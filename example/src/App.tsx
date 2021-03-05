2385fb008782418df9bf4ed97f132a25ba5382b1
Ricardo Malagon <ricardo@ricardomalagon.com>
2021-03-04 22:24:35 -0400

    update example

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,23 +1,29 @@
 import React from 'react'
 import axios from 'axios'
+import PostRenderer from '@upnread/post-renderer'
 
-import PostReader from '@upnread/post-renderer'
+async function getPostById() {
+  const post_id = 0 // This is the post id to bring the post that you want.
+  const token = '' // This is where you would add your private blog token that is provide it into your account.
+  const endpoint = 'https://api.upnread.com/api/post/get_post'
+
+  const response = await axios.get(endpoint, { params: { token, post_id } })
+  return response
+}
 
 const App = () => {
   const [post, setPost] = React.useState({ body: '' })
 
   React.useEffect(() => {
     async function getPost() {
-      const { data } = await axios.get(
-        'http://127.0.0.1:8000/api/post/test_post'
-      )
+      const { data } = await getPostById()
       setPost(data)
     }
 
     getPost()
   }, [])
 
-  return <PostReader post={post.body} />
+  return <PostRenderer post={post.body} />
 }
 
 export default App
