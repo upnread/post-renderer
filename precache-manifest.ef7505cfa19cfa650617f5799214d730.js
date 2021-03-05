a9ce06c4b0811b245145f31190768355c98a1f22
Ricardo Malagon <ricardo@ricardomalagon.com>
2021-03-04 22:41:47 -0400

    Updates

---
+++ precache-manifest.ef7505cfa19cfa650617f5799214d730.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2701e6f5677afa4217e804254792110c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "56443d47ae98776f2ee0",
+    "url": "./static/css/2.9d1693be.chunk.css"
+  },
+  {
+    "revision": "8d79bf5702290b5ce743",
+    "url": "./static/css/main.42cc8b22.chunk.css"
+  },
+  {
+    "revision": "56443d47ae98776f2ee0",
+    "url": "./static/js/2.536c5af4.chunk.js"
+  },
+  {
+    "revision": "73cdf50a93bc367aba761b39c76cd42a",
+    "url": "./static/js/2.536c5af4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8d79bf5702290b5ce743",
+    "url": "./static/js/main.fed7efe1.chunk.js"
+  },
+  {
+    "revision": "f00f5cda1b81933d1100",
+    "url": "./static/js/runtime-main.e7fc1bf6.js"
+  }
+]);
