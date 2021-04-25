7e433c2230c9369b82f9ed6ca4b3126e313275b8
Ricardo Malagon <ricardo@ricardomalagon.com>
2021-04-25 12:07:43 -0400

    add roll up plugin

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -3,8 +3,8 @@ import axios from 'axios'
 import PostRenderer from '@upnread/post-renderer'
 
 async function getPostById() {
-  const post_id = 0 // This is the post id to bring the post that you want.
-  const token = '' // This is where you would add your private blog token that is provide it into your account.
+  const post_id = 751 // This is the post id to bring the post that you want.
+  const token = 'R1W0bW8R8DHVQ9G9E9NSU88UAAvSxi' // This is where you would add your private blog token that is provide it into your account.
   const endpoint = 'https://api.upnread.com/api/post/get_post'
 
   const response = await axios.get(endpoint, { params: { token, post_id } })
